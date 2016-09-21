9f186ae5331db501bd8dadd680925a2664082829
elder-of-ozone <a.fordham@iinet.net.au>
2016-09-21 19:27:05 +0000

    moved a few functions around

--- User.py
+++ User.py
@@ -14,5 +14,18 @@ class User():
 
    # The following functions will edit this field 
     
-    def getUserPlanets(self):
+    def getUsersPlanets(self, user, planets, pid=[]):
+        planets_id = [x for x in user["planetID"]]
+        plist = []
+        for i in planet_id:
+            plist.append(planets[i])
+
+        return plist
+
+    def grabPlanetObj(user, fleets, fid=[])
+        fleet_id = [ x for x in user["fleetID"]]
+        flist = []
+        for i in fleet_id:
+            flist.append(fleet[i])
+
 
