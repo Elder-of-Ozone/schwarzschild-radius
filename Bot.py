1368284667a80539cd58d1b9e055021d0d772e5f
elder-of-ozone <a.fordham@iinet.net.au>
2016-09-28 15:41:15 +0000

    Added class to initialize bots

---
+++ Bot.py
@@ -0,0 +1,21 @@
+
+
+class Bot(self):
+    """
+    This class handles all instances of the bots in the game.
+    Name: Bot1, Bot2, Bot3???? 
+    Mode:   Exploring
+            Expanding
+            Attacking
+            Defending
+
+    Bot begins by exploring. Sending out probes to nearby systems.
+    The bot then scans() system for suitable planets and adds them to list.
+    SuitablePlanetList contains at least 10 suitable planets. The bot
+    sends outposts to two planets at the top of list.
+
+    Once established, needs to build mines and farm
+
+
+
+    """
